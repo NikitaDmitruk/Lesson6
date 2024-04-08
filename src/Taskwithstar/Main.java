18e4f8061bced3bc16b170cf14a651dad937e34f
NikitaDmitruk <neomouz@icloud.com>
2024-04-08 16:02:02 +0300

    Изменил комментарии, убрал присвоение значения sum [0][0], т.к. там и так записан 0.Немного обновил офомрление кода в main.

--- src/Taskwithstar/Main.java
+++ src/Taskwithstar/Main.java
@@ -1,18 +1,15 @@
 package Taskwithstar;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean success = false;
-        Atm Atm = new Atm(2, 0, 0);
+        Atm Atm = new Atm(0, 0, 0);
         Atm.AddCash();
         System.out.println("Введите сумму, которую хотите снять: ");
-        success = Atm.Withdrawn(scanner.nextInt());
-        if (success == false) {
+        boolean success = Atm.Withdrawn(scanner.nextInt());
+        if (!success) {
             System.out.println("Выдать сумму имеющимеся купюрами невозможно!");
         }
     }
